587626dfee833b6e12676dcc14c68317316e7fb2
ettoregiorgio <20026147@studenti.uniupo.it>
2021-07-19 15:49:59 +0200

    ciao

--- src/main/java/RESTService/RESTPrenotazione.java
+++ src/main/java/RESTService/RESTPrenotazione.java
@@ -22,7 +22,7 @@ public class RESTPrenotazione {
         public static void REST(Gson gson, String baseURL){
 
             GestionePrenotazione ReservationDao = new GestionePrenotazione();
-
+           //bellino
             // get all the tasks
             get(baseURL + "/reservations", (request, response) -> {
                 // set a proper response code and type
